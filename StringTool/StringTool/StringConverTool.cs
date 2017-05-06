32f1cddbcae5db4e57e6ed44af37bb0dbc6171a9
yjq <648070334@qq.com>
2017-05-06 15:58:54 +0800

    第一次提交

---
+++ StringTool/StringTool/StringConverTool.cs
@@ -0,0 +1,44 @@
+﻿//文件名称（File Name）                 StringConverTool.cs
+//作者(Author)                          yjq
+//日期（Create Date）                   2017.5.5
+//修改记录(Revision History)
+//    R1:
+//        修改作者:
+//        修改日期:
+//        修改原因:
+//    R2:
+//        修改作者:
+//        修改日期:
+//        修改原因:
+//**************************************************************
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace StringTool
+{
+    /// <summary>
+    /// 字符串转换工具类
+    /// </summary>
+    public class StringConverTool
+    {
+        public static int StringToInt(object strValue)
+        {
+            int defValue = -1;
+            try
+            {
+                if (strValue!=null&& string.IsNullOrWhiteSpace(strValue))
+                {
+
+                }
+            }
+            catch (Exception e)
+            {
+                throw e;
+            }
+            return defValue;
+        }
+    }
+}
---
+++ StringTool/StringTool/StringTool.cs
@@ -0,0 +1,63 @@
+﻿//文件名称（File Name）                 StringTool.cs
+//作者(Author)                          yjq
+//日期（Create Date）                   2017.5.5
+//修改记录(Revision History)
+//    R1:
+//        修改作者:
+//        修改日期:
+//        修改原因:
+//    R2:
+//        修改作者:
+//        修改日期:
+//        修改原因:
+//**************************************************************
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Text.RegularExpressions;
+using System.Threading.Tasks;
+
+namespace StringTool
+{
+    /// <summary>
+    /// 字符串工具类
+    /// </summary>
+    public class StringTool
+    {
+        public static string[] SplitString(string strContent, string strSplit)
+        {
+            string[] result = new string[] { };
+            try
+            {
+                if (!string.IsNullOrWhiteSpace(strContent))
+                {
+                    if (!string.IsNullOrWhiteSpace(strSplit))
+                    {
+                        int i = strContent.IndexOf(strSplit);
+                        if (strContent.IndexOf(strSplit) > 0)
+                        {
+                            result = Regex.Split(strContent, @strSplit.Replace(".", @"\."));
+                        }
+                        else
+                        {
+                            //result = { strContent };
+                        }
+                    }
+                    else {
+                        throw new Exception();
+                    }
+                }
+                else
+                {
+                    throw new Exception();
+                }
+            }
+            catch (Exception e)
+            {
+                throw e;
+            }
+            return result;
+        }
+    }
+}
